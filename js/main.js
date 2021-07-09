67ac15ff86158775aa5545aaf6744165d5d287b9
dendenwav <raphael320coutaud@hotmail.com>
2021-07-09 10:45:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,8 +8,10 @@ let chrono;
 let button = document.querySelector('#firing-button');
 let span = document.querySelector('span');
 let rocket = document.querySelector('#rocket');
+let width = window.innerWidth;
+let height = window.innerHeight;
+let ciel = document.querySelector('main');
 
-button.addEventListener('click', decollage);
 /***********************************************************************************/
 /* ********************************** FONCTIONS ************************************/
 /***********************************************************************************/
@@ -33,6 +35,33 @@ function chronoLaunch() {
       rocket.classList.add('tookOff');
   }
 }
+
+function randomStar() {
+  let tab = ['tiny', 'normal', 'big'];
+  for (let i = 0 ; i<150; i++) {
+    let randomSize = tab[Math.floor(Math.random() *3)];
+    let randomWidth = Math.floor(Math.random() * width);
+    let randomHeight = Math.floor(Math.random() * height);
+    let star = document.createElement('div');
+    star.classList.add('star');
+    star.style.left = randomWidth + 'px';
+    star.style.top = randomHeight + 'px';
+    star.classList.add(randomSize);
+    ciel.appendChild(star);
+  }
+}
+
 /************************************************************************************/
 /* ******************************** CODE PRINCIPAL **********************************/
 /************************************************************************************/
+
+window.addEventListener('DOMContentLoaded', function() {
+  randomStar();
+  button.addEventListener('click', decollage);
+});
+
+
+
+
+
+
